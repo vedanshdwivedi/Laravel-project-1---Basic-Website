046b592fa59b62309472bb1a7883b31b0936becc
vedanshdwivedi <vedanshdwivedi0@gmail.com>
2018-08-28 13:29:50 +0530

    v0.0.4
    Connected to database 'basicwebdev'
    Contact Form Completed

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -12,6 +13,15 @@ class MessagesController extends Controller
     		'email' => 'required'
     	]);
 
-    	return 'SUCCESS';
+    	//Create New Message
+    	$message = new Message;
+    	$message->name = $request->input('name');
+    	$message->email = $request->input('email');
+    	$message->message = $request->input('message');	
+    	//Save Message
+    	$message->save();
+
+    	//Redirect
+    	return redirect('/')->with('success','Message Sent');
     }
 }
